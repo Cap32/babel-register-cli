c0d0a5b5f9ae9b9f75ba62dfd1a4e505e41db6e8
Webb Peng <webb.png@gmail.com>
2017-03-05 01:07:06 +0800

    optimized `ipc` stdio

--- test/index.js
+++ test/index.js
@@ -1,14 +1,34 @@
 
-const execSync = require('child_process').execSync;
+const childProcess = require('child_process');
 
 const babelRegisterCli = '../bin/babel-register.js';
 
 describe('babel-node-cli', () => {
 	it('run', () => {
-		const result = execSync(`${babelRegisterCli} ./source -r shit`, {
+		const result = childProcess.execSync(`${babelRegisterCli} ./source`, {
 			cwd: __dirname,
 			encoding: 'utf8',
 		});
 		result && console.log(result);
+	});
+
+	it('ipc', (done) => {
+		const child = childProcess.spawn(babelRegisterCli, ['./source'], {
+			cwd: __dirname,
+			stdio: ['ipc', 'pipe', 'pipe'],
+		});
+		child.stdout.on('data', (buf) => {
+			console.log(buf.toString());
+		});
+		child.stderr.on('data', (buf) => {
+			console.warn(buf.toString());
+		});
+		child.on('error', (err) => {
+			console.error(err);
+		});
+		child.on('exit', () => {
+			done();
+		});
+		child.disconnect();
 	});
 });
